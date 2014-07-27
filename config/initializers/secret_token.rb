d656ee46d476004fedc864499fe22d3833cc1c41
jamintz <jasonamintz@gmail.com>
2014-07-27 19:10:00 -0400

    test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sixpack::Application.config.secret_token = '81e51a9feb2173142d805078fd04d1fcac6a6c4329c80a030b2ee4ff13ee640864144c845484fd8d3f535083439623bbb5b4a6c2b04f6bbbce34c74a6eb34975'
